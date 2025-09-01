4cfec104ecc2b4e3c8c7d6b46603c0c4ad4cf3b4
Mafikul Islam <mdmafikulislam5320@gmail.com>
2025-09-02 02:01:12 +0600

    User , login api create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
@@ -17,6 +18,43 @@ const client = new MongoClient(process.env.uri, {
 
 async function run() {
   try {
+    const database = client.db("SwiftShop")
+    const userCollection = database.collection("Users")
+
+
+    app.post("/user", async (req, res) => {
+      const data = req.body
+      try {
+        const result = await userCollection.insertOne(data);
+        res.status(201).send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.get("/login", async (req, res) => {
+      try {
+        const { email, password } = req.query;
+        console.log(email, password)
+
+        const user = await userCollection.findOne({ email, password });
+        const token = jwt.sign(
+          { "email": email },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "1h" }
+        );
+
+        if (!user) {
+          return res.status(401).send({ message: "Invalid credentials" });
+        }
+
+        res.send({ message: "Login successful", user, token });
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
